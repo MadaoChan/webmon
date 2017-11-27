de42e9df8a679a5b10132f1fac965a9a87d0cb5d
MadaoChan <uc0096@aliyun.com>
2017-11-27 21:50:24 +0800

    加入重试机制。

--- src/com/madaochan/webmon/connection/Connection.java
+++ src/com/madaochan/webmon/connection/Connection.java
@@ -18,6 +18,20 @@ public class Connection {
     public static String ABNORMAL_STATE = "异常";
     public static String QUERYING_STATE = "查询中…";
 
+    // 默认30秒超时
+    private final int DEFAULT_TIME_OUT_MS = 1000 * 30;
+
+    // 默认3次重试
+    private final int DEFAULT_RETRY_TIMES = 3;
+
+    private int timeOutMs;
+    private int retryTimes;
+
+    public Connection() {
+        timeOutMs = DEFAULT_TIME_OUT_MS;
+        retryTimes = DEFAULT_RETRY_TIMES;
+    }
+
     /**
      * 获取指定URL的响应码（默认5s超时时间）
      * @param tag 网址标签
@@ -25,8 +39,7 @@ public class Connection {
      * @return 查询结果
      */
     public String getResponseCode(String tag, String urlStr) {
-        // 默认5秒超时
-        return getResponseCode(tag, urlStr, 1000 * 5);
+        return getResponseCode(tag, urlStr, timeOutMs, retryTimes);
     }
 
     /**
@@ -34,30 +47,40 @@ public class Connection {
      * @param tag 网址标签
      * @param urlStr 网址url
      * @param timeOutMs 超时时间
+     * @param retryTimes 重试次数
      * @return 查询结果
      */
-    public String getResponseCode(String tag, String urlStr, int timeOutMs) {
-        String response;
+    public String getResponseCode(String tag, String urlStr, int timeOutMs, int retryTimes) {
+        String response = null;
         HttpURLConnection conn = null;
-        try {
-            URL url = new URL(urlStr);
-            long startTime = System.currentTimeMillis();
-            conn = (HttpURLConnection)url.openConnection();
-            conn.setDoOutput(true);
-            conn.setConnectTimeout(timeOutMs);
-            conn.connect();
-            long resTime = System.currentTimeMillis() - startTime;
-            response = TimeUtils.getCurrentTime() + "\t" + translateResCode(conn.getResponseCode()) +
-                    "\t响应时间:" + resTime + "ms\t\t" + tag + "\t\t" + urlStr;
-            conn.disconnect();
-        } catch (SocketTimeoutException e) {
-            response = TimeUtils.getCurrentTime() + "\t【异常】连接超时" + "\t\t\t" + tag + "\t\t" + urlStr;
-        } catch (IOException e) {
-            e.printStackTrace();
-            response = TimeUtils.getCurrentTime() + "\t【异常】无法建立连接" + "\t\t\t" + tag + "\t\t" + urlStr;
-        } finally {
-            if (conn != null) {
-                conn.disconnect();
+
+        boolean isSuccess = false;
+
+        for (int i = 0; i < retryTimes && !isSuccess; i++) {
+            if (i > 0) {
+                System.out.println(TimeUtils.getCurrentTime() + " " + tag + " 重试" + i);
+            }
+            try {
+                URL url = new URL(urlStr);
+                long startTime = System.currentTimeMillis();
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+                conn.setConnectTimeout(timeOutMs);
+                conn.connect();
+                long resTime = System.currentTimeMillis() - startTime;
+                    response = TimeUtils.getCurrentTime() + "\t" + translateResCode(conn.getResponseCode()) +
+                            "\t响应时间:" + resTime + "ms\t\t" + tag + "\t\t" + urlStr;
+                    isSuccess = true;
+                    conn.disconnect();
+                } catch (SocketTimeoutException e) {
+                    response = TimeUtils.getCurrentTime() + "\t【异常】连接超时" + "\t\t\t" + tag + "\t\t" + urlStr;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    response = TimeUtils.getCurrentTime() + "\t【异常】无法建立连接" + "\t\t\t" + tag + "\t\t" + urlStr;
+                } finally {
+                    if (conn != null) {
+                    conn.disconnect();
+                }
             }
         }
         return response;
